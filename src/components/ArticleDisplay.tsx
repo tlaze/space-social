63fb29a1ed40c84ac94ebe457fa791d70a08a456
RECamp88 <recamp88@gmail.com>
2023-07-16 21:54:22 -0500

    Added in some functions and useSTate to the ArticalView, added the Article model, and the ArticleDisplay component.

---
+++ src/components/ArticleDisplay.tsx
@@ -0,0 +1,17 @@
+import { Article } from "../models/Article"
+
+
+interface ArticleProps{
+    article:Article
+}
+// This componenet will be render on the ArticleView Page
+// this has to show the article that is located on a different website. 
+export function ArticleDisplay(props:ArticleProps){
+
+    return(
+
+        <>
+        
+        </>
+    )
+}
--- src/pages/ArticalView.tsx
+++ src/pages/ArticalView.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import {ArticleDisplay}  from '../components/ArticleDisplay';
+import { Article } from '../models/Article';
 
-export function ArticleView(){
+interface ArticleProps{
+  article:Article
+}
 
+export function ArticleView(article:ArticleProps){
+    const [selectionState, updateSelectionState] = useState(0);
+    const [articleSelected, updateArticleSelected] = useState({id:0, url:''});
+
+
+    function getArticleAPI(){
+      let chosenArticle = Axios.get("https://api.spaceflightnewsapi.net//v4/articles/" + selectionState).then((response) =>
+      {
+        let apiArticle:Article = {
+          id: response.data.id, 
+          url: response.data.url,
+          title: '',
+          image_url: '',
+          summary: ''
+        }
+        updateArticleSelected(apiArticle)
+      });
+    }
     return(
 
         <>
-        {/* 
+          <div className="article-view">
+            <ArticleDisplay article={undefined}></ArticleDisplay>
+          </div>
+        </>
+    );
+
+    {/* 
         First part of this view is getting the specific article to load
         - the id of the article should be brought in because if the user clicking 
           to view that particular article from the homepage. 
-        - the view will load the img in the top left of the div with the text 
-          including the title wrapped around it.  
-        
+                
         Second part of this view will be the comments block.  
         - this will be made as a separate component and brought into this view.
         - the user can only make a comment if they are logged in. This doesn't have 
@@ -24,6 +51,4 @@ export function ArticleView(){
         
         Optional forth part if time allows will be giving a reader the option to click a 
         thumbs up or down for the article and it show the count beside it. */}
-        </>
-    );
 }
