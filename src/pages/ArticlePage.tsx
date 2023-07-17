4f5702e159018942c360bb2910501cffb133b50f
ErichD <dehavenerich@gmail.com>
2023-07-17 12:19:40 -0700

    new routes

---
+++ src/pages/ArticlePage.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+export default function ArticlePage(){
+    const router = useNavigate();
+
+    return<>
+        
+        <h1>Article page placeholder</h1>
+    </>
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+
+export default function HomePage(){
+    const router = useNavigate();
+
+    return<>
+        
+        <h1>The home page placeholder</h1>
+    </>
+}
